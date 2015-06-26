730881f1541d10a0443788ef05d6cc2960591cc8
unknown <contact@codezur.com>
2015-06-25 21:10:45 -0700

    nuevo merge

--- module/Usuarios/config/module.config.php
+++ module/Usuarios/config/module.config.php
@@ -18,11 +18,11 @@ return array(
     ),
     'db' => array(
         'driver' =>'Pdo',
-//      'dsn' =>'mysql:dbname=tu_tesis_en_linea;host=localhost',
-        'dsn' => 'mysql:dbname=tesis_en_linea;host=127.0.0.1',
+      'dsn' =>'mysql:dbname=tu_tesis_en_linea;host=localhost',
+//        'dsn' => 'mysql:dbname=tesis_en_linea;host=127.0.0.1',
         'username' => 'root',
-//      'password' => 'talinaes',
-        'password' => 'lsnsdmsdp1',
+      'password' => 'talinaes',
+//        'password' => 'lsnsdmsdp1',
         'driver_options' => array(
             PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
         ),
