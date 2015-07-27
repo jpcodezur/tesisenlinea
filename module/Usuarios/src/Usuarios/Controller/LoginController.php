dcd7dfca72362c190c6ee0bac45c9e6ae5e61248
unknown <contact@codezur.com>
2015-07-26 17:36:23 -0700

    maquetado v2

--- module/Usuarios/src/Usuarios/Controller/LoginController.php
+++ module/Usuarios/src/Usuarios/Controller/LoginController.php
@@ -54,7 +54,7 @@ class LoginController extends AbstractActionController {
     }
 
     public function indexAction() {
-
+        
         if ($this->login->isLoggedIn()) {
             return $this->redirect()->toRoute('usuarios', array(
                         'controller' => 'index',
@@ -63,10 +63,12 @@ class LoginController extends AbstractActionController {
         }
 
         $form = new Login("login");
-
+        
         $modeView = new ViewModel(array("form" => $form));
 
         $this->layout()->setTemplate('usuarios/login/login.phtml');
+        
+        $this->layout()->mensajes = $this->flashMessenger()->getMessages();
 
         return $modeView;
     }
@@ -109,34 +111,37 @@ class LoginController extends AbstractActionController {
             if (!isset($miSession->usuario))
                 $miSession->usuario = $usuario;
 
-            $this->layout()->mensaje = "LoginCorrecto!!!";
-            $this->layout()->colorMensaje = "green";
+            $this->flashMessenger()->addMessage("<p class='success-label mensaje-label' style='color:white;background-color:green;'>Login Correcto</p>");
 
             return $this->redirect()->toRoute('usuarios', array(
                         'controller' => 'index',
-                        'action' => 'index'
+                        'action' => 'index',
             ));
+            
         } else {
-//            $mensaje = "<p style='color:red'>Datos incorrectos.</p>";
-//                    $this->layout()->setTemplate('usuarios/login/login.phtml');
-//                    return new ViewModel(array("mensaje" => $mensaje));
-
+            
+            $this->flashMessenger()->addMessage("<p class='error-label mensaje-label' style='color:white;background-color:red'>Login Incorrecto</p>");
+            
             return $this->redirect()->toRoute('usuarios', array(
                         'controller' => 'login',
-                        'action' => 'index'
+                        'action' => 'index',
             ));
         }
     }
 
     public function logoutAction() {
         $this->login->logout();
+        $this->flashMessenger()->clearCurrentMessages();
         return $this->redirect()->toRoute('usuarios', array('controller' => 'login', 'action' => 'index'));
     }
 
     public function registrarseAction() {
-
+        $mensaje = "";
+        
+        $error = false;
+        
         if ($this->request->isPost()) {
-
+            
             $user = new Usuario();
             $email = $this->getRequest()->getPost('email-user', null);
             $re_email = $this->getRequest()->getPost('reemail-user', null);
@@ -147,26 +152,41 @@ class LoginController extends AbstractActionController {
             $tipo = $this->getRequest()->getPost('level-user', null);
 
             if (isset($email, $re_email)) {
-                if ($email !== $re_email) {
-                    $mensaje = "<p style='color:red'>Error: La dirección de correo debe coincidir en ambos campos.</p>";
-                    $this->layout()->setTemplate('usuarios/login/login.phtml');
-                    return new ViewModel(array("mensaje" => $mensaje));
+                if ($email != $re_email) {
+                    $this->flashMessenger()->addMessage("<p  class='error-label mensaje-label' style='color:white;background-color:red'>Error: La dirección de correo debe coincidir en ambos campos.</p>");
+                    $error = true;
                 }
             }
 
             if (isset($password, $re_password)) {
-                if ($password !== $re_password) {
-                    $mensaje = "<p style='color:red'>Error: La contraseña debe coincidir en ambos campos.</p>";
-                    $this->layout()->setTemplate('usuarios/login/login.phtml');
-                    return new ViewModel(array("mensaje" => $mensaje));
+                
+                if(strlen($password) < 4){
+                    $error = true;
+                    $this->flashMessenger()->addMessage("<p  class='error-label mensaje-label' style='color:white;background-color:red'>Error: El largo de la contraseña no debe ser menor a 6 caracteres.</p>");
                 }
+                
+                if(strlen($re_password) < 4){
+                    $error = true;
+                    $this->flashMessenger()->addMessage("<p  class='error-label mensaje-label' style='color:white;background-color:red'>Error: El largo de la confirmacion de la contraseña no debe ser menor a 6 caracteres.</p>");
+                }
+                
+                if(!$password || !$re_password){
+                    $error = true;
+                    $this->flashMessenger()->addMessage("<p  class='error-label mensaje-label' style='color:white;background-color:red'>Error: Debe ingresar una contraseña.</p>");
+                }
+                
+                if ($password != $re_password) {
+                    $error = true;
+                    $this->flashMessenger()->addMessage("<p  class='error-label mensaje-label' style='color:white;background-color:red'>Error: La contraseña debe coincidir en ambos campos.</p>");
+                }
+            }else{
+                $error = true;
+                $this->flashMessenger()->addMessage("<p  class='error-label mensaje-label' style='color:white;background-color:red'>Error: Debe ingresar una contraseña.</p>");
             }
 
             if (!$this->noExisteEmail($email)) {
-                $this->layout()->setTemplate('usuarios/login/login.phtml');
-                return new ViewModel(array(
-                    "save" => false,
-                    "mensaje" => "<p style='color:red'>Ya existe un usuario con ese email.</p>"));
+                $this->flashMessenger()->addMessage("<p  class='error-label mensaje-label' style='color:white;background-color:red'>Ya existe un usuario con ese email.</p>");
+                $error = true;
             }
 
             $user->setEmail($email);
@@ -176,52 +196,53 @@ class LoginController extends AbstractActionController {
             $user->setTipo($tipo);
             $user->setEstado(0);
             $user->setClaveActivacion(md5($email));
+            
+            if(!$error){
+                if ($this->usuarioDao->guardar($user)) {
 
-            if ($this->usuarioDao->guardar($user)) {
+                    //Envio de link de activacion
+    //                $query = "UPDATE usuarios SET estado = 1 WHERE email ='" . $_POST["email-user"] . "'";
+    //                $current_id = $this->usuarioDao->update($user);
+    //                
+    //                if (!empty($current_id)) {
+    //                    $actual_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]" . "activar.php?email=" . $user->getEmail();
+    //                    $toEmail = $_POST["userEmail"];
+    //                    $subject = "User Registration Activation Email";
+    //                    $content = "Clickear en el link para activar tu cuenta. <a href='" . $actual_link . "'>" . $actual_link . "</a>";
+    //                    $mailHeaders = "From: Admin\r\n";
+    //
+    //                    $unEmail = new SendEmail($toEmail, "atfede@gmail.com", $subject);
+    //                    $unEmail->sendEmail($body);
+    //                    if (mail($toEmail, $subject, $content, $mailHeaders)) {
+    //                        $message = "You have registered and the activation mail is sent to your email. Click the activation link to activate you account.";
+    //                    }
+    //                    unset($_POST);
+    //                }
+                    //Envio de link prueba
 
-                //Envio de link de activacion
-//                $query = "UPDATE usuarios SET estado = 1 WHERE email ='" . $_POST["email-user"] . "'";
-//                $current_id = $this->usuarioDao->update($user);
-//                
-//                if (!empty($current_id)) {
-//                    $actual_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]" . "activar.php?email=" . $user->getEmail();
-//                    $toEmail = $_POST["userEmail"];
-//                    $subject = "User Registration Activation Email";
-//                    $content = "Clickear en el link para activar tu cuenta. <a href='" . $actual_link . "'>" . $actual_link . "</a>";
-//                    $mailHeaders = "From: Admin\r\n";
-//
-//                    $unEmail = new SendEmail($toEmail, "atfede@gmail.com", $subject);
-//                    $unEmail->sendEmail($body);
-//                    if (mail($toEmail, $subject, $content, $mailHeaders)) {
-//                        $message = "You have registered and the activation mail is sent to your email. Click the activation link to activate you account.";
-//                    }
-//                    unset($_POST);
-//                }
-                //Envio de link prueba
+                    $body = "click en el link para activar";
+                    $body .= "<a href='http://64.22.74.53/tutesisenlinea/public/usuarios/login/validarclave?clave=" . $user->getClaveActivacion() . "'>Activar</a>";
+                    $to = $user->getEmail();
+                    $asunto = "Tu Tesis en Linea - Activar cuenta";
 
-                $body = "click en el link para activar";
-                $body .= "<a href='http://64.22.74.53/tutesisenlinea/public/usuarios/login/validarclave?clave=" . $user->getClaveActivacion() . "'>Activar</a>";
-                $to = $user->getEmail();
-                $asunto = "Tu Tesis en Linea - Activar cuenta";
+                    $unEmail = new SendEmail($to, "testcodezur@gmail.com", $asunto);
+                    $unEmail->sendEmail($body);
+                    $this->flashMessenger()->clearCurrentMessages();
+                    $mensaje = "<p  class='success-label mensaje-label' style='color:white;background-color:green'>Usuario creado satisfactoriamente. Verifique su email para activar su cuenta.</p>";
+                    $this->flashMessenger()->addMessage($mensaje);
+                } else {
+                    $error = true;
+                    $this->flashMessenger()->clearCurrentMessages();
+                    $this->flashMessenger()->addMessage("<p  class='error-label mensaje-label' style='color:white;background-color:red'>Error creando la cuenta</p>");
 
-                $unEmail = new SendEmail($to, "testcodezur@gmail.com", $asunto);
-                $unEmail->sendEmail($body);
-
-                $mensaje = "<p style='color:green'>Usuario creado satisfactoriamente. Verifique su email para activar su cuenta.</p>";
-
-                $this->layout()->setTemplate('usuarios/login/login.phtml');
-                return new ViewModel(array("mensaje" => $mensaje));
-            } else {
-                $mensaje = "<p style='color:red'>Error creando la cuenta</p>";
-
-                $this->layout()->setTemplate('usuarios/login/login.phtml');
-                return new ViewModel(array("mensaje" => $mensaje));
+                }
             }
         }
-
-//      $this->layout()->setTemplate('usuarios/login/success.phtml');
-        $this->layout()->setTemplate('usuarios/login/login.phtml');
-//        return new ViewModel(array("mensaje" => "Hola")); //crear nuevo View
+        
+        return $this->redirect()->toRoute('usuarios', array(
+		'controller' => 'login',
+		'action' => 'index',
+	));
     }
 
     public function noExisteEmail($pEmail) {
@@ -251,7 +272,7 @@ class LoginController extends AbstractActionController {
         $mensaje = "";
 
         if ($activar) {
-            $mensaje = "<h2 style='color:green'>Usuario activado con exito</h2>";
+            $mensaje = "<h2  class='success-label mensaje-label' style='color:white;background-color:green'>Usuario activado con exito</h2>";
         }
 
         return $this->redirect()->toRoute('usuarios', array(
@@ -267,7 +288,7 @@ class LoginController extends AbstractActionController {
         $email = $this->getRequest()->getPost('email-user', null);
 
         if ($email == "") {
-            $mensaje = "<p style='color:red'></p>";
+            $mensaje = "";
             $this->layout()->setTemplate('usuarios/login/recuperarpass.phtml');
             return new ViewModel(array("mensaje" => $mensaje));
         }
@@ -295,7 +316,7 @@ class LoginController extends AbstractActionController {
 
         $this->usuarioDao->updatePassword($user);
 
-        $mensaje = "<p style='color:green'>Verifique su email para recuperar su contraseña.</p>";
+        $mensaje = "<p  class='success-label mensaje-label' style='color:white;background-color:green'>Verifique su email para recuperar su contraseña.</p>";
 
         $this->layout()->setTemplate('usuarios/login/recuperarpass.phtml');
         return new ViewModel(array("mensaje" => $mensaje));
